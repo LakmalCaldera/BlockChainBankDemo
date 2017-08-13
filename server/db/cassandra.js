429b87c631c5d8ca05f30e6754e3a9451ac0c924
Lakmal Caldera <lakmal.developer@gmail.com>
2017-08-13 21:30:10 +0530

    replaced double quotes with back ticks

--- server/db/cassandra.js
+++ server/db/cassandra.js
@@ -4,7 +4,7 @@ const client = new cassandra.Client({ contactPoints: ['127.0.0.1']})
 client.connect().then(() => {
     console.log("Successfully connected to Cassandra db")
 }).catch((ex) => {
-    console.log("Error connecting to Cassandra db: ${ex}");
+    console.log(`Error connecting to Cassandra db: ${ex}`);
 })
 
 module.exports = client
